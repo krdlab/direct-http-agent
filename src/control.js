45707decd6f13912bfff62e0e052903cd0708fee
krdlab <krdlab@gmail.com>
2017-09-08 03:48:40 +0900

    Remove duplicate checking

--- src/control.js
+++ src/control.js
@@ -1,9 +1,8 @@
 // file: src/control.js
 const router = require('express').Router();
-const auth = require('./auth');
 const model  = require('./model');
 
-router.post('/restart', auth.checkApiToken, async (req, res) => {
+router.post('/restart', async (req, res) => {
   await model.restartClient(req.user);
   res.sendStatus(200);
 });
