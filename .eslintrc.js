2322badddf0bad359638895d0fdfe933bb53fda7
krdlab <krdlab@gmail.com>
2017-09-10 21:54:39 +0900

    Install and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "indent": ["error", 2],
+        "no-console": 0
+    }
+};
