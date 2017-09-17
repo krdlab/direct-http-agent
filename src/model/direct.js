2f27d430fa8b29bb6cdfd4f8389756c081cc539f
krdlab <krdlab@gmail.com>
2017-09-17 11:42:53 +0900

    Perform refactoring of worker process

--- src/model/direct.js
+++ src/model/direct.js
@@ -24,61 +24,112 @@ const dispatch = (msg) => {
   }
 };
 
-let direct = null;
+let client = null;
 
 const start = (msg, user) => {
-  const d = DirectAPI.getInstance();
-  d.setOptions({
-    host: 'api.direct4b.com',
-    endpoint: 'wss://api.direct4b.com/albero-app-server/api',
-    access_token: user.directApiToken
-    // TODO: storage_path
-  });
-  d.listen();
-  direct = d; // FIXME: global variable
+  client = new Client(user);
   process.send({method: msg.method, result: 'OK'});
 };
 
-const asc   = (a, b) => ((a.high - b.high) || (a.low - b.low));
-
 const getDomains = (msg) => {
-  const ds = direct.data.getDomains();
-  const str = direct.stringifyInt64;
-  const result = ds.sort((a, b) => asc(a.id, b.id)).map(d => ({id: str(d.id), name: d.name}));
+  const result = client.getDomains();
   process.send({method: msg.method, result});
 };
 
 const getTalks = (msg, domainId) => {
-  const ts = direct.data.getTalks();
-  const str = direct.stringifyInt64;
-  const result = ts
-    .filter((t) => str(t.domainId) === domainId)
-    .sort((a, b) => asc(a.id, b.id))
-    .map(t => ({id: str(t.id), name: t.name, type: t.type[0], userIds: (t.userIds || []).map(i => str(i))}));
+  const result = client.getTalks(domainId);
   process.send({method: msg.method, result});
 };
 
-const existsTalk = (domainId, talkId) => {
-  const ts = direct.data.getTalks();
-  const str = direct.stringifyInt64;
-  const result = ts
-    .filter((t) => str(t.domainId) === domainId)
-    .filter((t) => str(t.id) === talkId);
-  return result.length > 0;
-};
-
-const makeIdStr = (d, s) => {
-  const i = d.parseInt64(s);
-  return `_${i.high}_${i.low}`;
-};
-
 const sendTextMessage = (msg, domainId, talkId, content) => {
-  if (existsTalk(domainId, talkId)) {
-    const room = makeIdStr(direct, talkId);
-    const text = content.text || content;
-    direct.send({room}, text);
-    process.send({method: msg.method, result: 'OK'});
-  } else {
-    process.send({method: msg.method, error: 'NotFound'});
-  }
+  client.sendTextMessage(domainId, talkId, content.text || content)
+    .then(() => process.send({method: msg.method, result: 'OK'}))
+    .catch(e => process.send({method: msg.method, error: e}));
 };
+
+// ----
+
+const idAsc   = (a, b) => ((a.high - b.high) || (a.low - b.low));
+const byIdAsc = (a, b) => idAsc(a.id, b.id)
+
+class Client {
+  constructor(user) {
+    this.user = user;
+    this.client = Client._startAs(user);
+
+    this._strId = this._strId.bind(this);
+    this._makeIdStr = this._makeIdStr.bind(this);
+  }
+
+  static _startAs(user) {
+    const d = DirectAPI.getInstance();
+    d.setOptions({
+      host: 'api.direct4b.com',
+      endpoint: 'wss://api.direct4b.com/albero-app-server/api',
+      access_token: user.directApiToken
+      // TODO: storage_path
+    });
+    d.listen();
+    return d;
+  }
+
+  _strId(i) {
+    return this.client.stringifyInt64(i);
+  }
+
+  _makeIdStr(s) {
+    const i = this.client.parseInt64(s);
+    return `_${i.high}_${i.low}`;
+  }
+
+  _getDomains() {
+    return this.client.data.getDomains();
+  }
+
+  _getTalks() {
+    return this.client.data.getTalks();
+  }
+
+  _existsTalkInDomain(talkId, domainId) {
+    const ts  = this._getTalks();
+    const str = this._strId;
+    const res = ts
+      .filter((t) => str(t.domainId) === domainId)
+      .filter((t) => str(t.id) === talkId);
+    return res.length > 0;
+  }
+
+  getDomains() {
+    const ds  = this._getDomains();
+    const str = this._strId;
+    const res = ds
+      .sort(byIdAsc)
+      .map(d => ({id: str(d.id), name: d.domainInfo.name}));
+    return res;
+  }
+
+  getTalks(domainId) {
+    const ts  = this._getTalks();
+    const str = this._strId;
+    const res = ts
+      .filter((t) => str(t.domainId) === domainId)
+      .sort(byIdAsc)
+      .map(t => ({
+        id: str(t.id),
+        name: t.name,
+        type: t.type[0],
+        userIds: (t.userIds || []).map(i => str(i))
+      }));
+    return res;
+  }
+
+  sendTextMessage(domainId, talkId, text) { // :: self => String -> String -> String -> Promise ()
+    if (this._existsTalkInDomain(talkId, domainId)) {
+      const room = this._makeIdStr(talkId);
+      this.client.send({room}, text);
+      return Promise.resolve('OK');
+    } else {
+      return Promise.reject('NotFound');
+    }
+  }
+}
